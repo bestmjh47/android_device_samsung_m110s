e86fa5067c7ffae39816721522bbfe54b22eb7d5
yorks <sch2307@naver.com>
2013-01-30 17:44:09 +0900

    cm.mk: 경로 및 기기명 수정

--- cm.mk
+++ cm.mk
@@ -8,13 +8,13 @@ PRODUCT_RELEASE_NAME := GalaxyS
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/galaxysmtd/full_galaxysmtd.mk)
+$(call inherit-product, device/samsung/m110s/full_galaxysmtd.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := galaxysmtd
 PRODUCT_NAME := cm_galaxysmtd
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9000
+PRODUCT_MODEL := SHW-M110S
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9000 TARGET_DEVICE=GT-I9000 BUILD_FINGERPRINT=samsung/GT-I9000/GT-I9000:2.3.5/GINGERBREAD/XXJVT:user/release-keys PRIVATE_BUILD_DESC="GT-I9000-user 2.3.5 GINGERBREAD XXJVT release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHW-M110S TARGET_DEVICE=SHW-M110S BUILD_FINGERPRINT=samsung/SHW-M110S/SHW-M110S:2.3.6/GINGERBREAD/WF07:user/release-keys PRIVATE_BUILD_DESC="SHW-M110S-user 2.3.6 GINGERBREAD WF07 release-keys"
